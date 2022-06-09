d9e80c9ade94199e9f4ce1bd908f93d11d16d324
harang90 <harang90@gmail.com>
2022-06-09 23:03:58 +0900

    [WIP] commit

--- src/main/java/com/nonononoki/alovoa/AlovoaApplication.java
+++ src/main/java/com/nonononoki/alovoa/AlovoaApplication.java
@@ -1,12 +1,16 @@
 package com.nonononoki.alovoa;
 
 import java.security.Security;
+import java.util.Arrays;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -22,5 +26,19 @@ public class AlovoaApplication {
 		
 		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(AlovoaApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 }
